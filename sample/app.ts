693bedd5fb788b5b1ee3fff118f1ad59e7d76f76
Myroslav Gavryliak <efristical@gmail.com>
2017-10-18 21:45:20 +0300

    buliding correct structure before sending to a backend

--- sample/app.ts
+++ sample/app.ts
@@ -11,10 +11,10 @@ const reader: any = new SampleReader(filePath);
 
 const file: { [prop: string]: IStatsChunk } = reader.read() ; // customizable user input
 // file is customer input
-// here we should use it and pass into out sdk
+// here we should use it and pass into our sdk
 const slice = (arrRef, start, end): IStatsChunk => {
 	const _listOfAllowedProps: string[] = ['extra', 'stat', 'time'];
-	let resultObj: IStatsChunk = {};
+	let resultObj: IStatsChunk = <IStatsChunk>{};
 	_listOfAllowedProps.forEach( prop => {
 		if (prop === 'time' && arrRef) {
 			resultObj[prop] = Array.prototype.slice.call(arrRef[prop], start, end);
@@ -22,7 +22,6 @@ const slice = (arrRef, start, end): IStatsChunk => {
 
 		if (prop === 'extra' && arrRef) {
 			resultObj[prop] = <IExtra>{};
-			console.log('arr ref: ', JSON.stringify(arrRef[prop]));
 			const listOfProps: string[] = Object.getOwnPropertyNames(arrRef[prop]);
 			listOfProps.forEach( _prop => {
 				if (Array.isArray( arrRef[prop][_prop] )) {
@@ -31,28 +30,22 @@ const slice = (arrRef, start, end): IStatsChunk => {
 					resultObj[prop][_prop] = arrRef[prop][_prop];
 				}
 			});
-		} else {
-			resultObj[prop] = {};
-		}
+		} 
 
 		if (prop === 'stat' && arrRef) {
-			console.log('in stat');
 			resultObj[prop] = resultObj[prop] || {};
 
 			const listOfProps: string[] = Object.getOwnPropertyNames(arrRef[prop]);
-			console.log('allowed list: ', listOfProps);
 			listOfProps.forEach( _prop => {
 				// console.log(`prop is ${prop}; _prop is ${_prop}; arrRef[prop] = ${arrRef[prop]}`);
 				resultObj[prop][_prop] = resultObj[prop][_prop] || <IConnection>{};
 				if (Array.isArray( arrRef[prop][_prop] )) {
-					console.log(`${_prop} is array`);
 					if (!Array.isArray(resultObj[prop][_prop])) {
-						resultObj[prop][_prop] = [ arrRef[prop][_prop].slice(start, end) ];
+						resultObj[prop][_prop] = arrRef[prop][_prop].slice(start, end);
 					} else {
 						Array.prototype.push.apply(resultObj[prop][_prop], arrRef[prop][_prop].slice(start, end));
 					}
 				} else {
-					console.log(`${_prop} is not an array`);
 					// prop stat _prop ConnAudio
 					const listOfProps: string[] = Object.getOwnPropertyNames(arrRef[prop][_prop]);
 					listOfProps.forEach( __prop => {
@@ -60,12 +53,12 @@ const slice = (arrRef, start, end): IStatsChunk => {
 						// __prop like bytesSent
 						if (Array.isArray(arrRef[prop][_prop][__prop])) {
 							if (!Array.isArray(resultObj[prop][_prop][__prop])) {
-								resultObj[prop][_prop][__prop] = [ arrRef[prop][_prop][__prop].slice(start, end) ];
+								resultObj[prop][_prop][__prop] = arrRef[prop][_prop][__prop].slice(start, end);
 							} else {
 								Array.prototype.push.apply(resultObj[prop][_prop][__prop], arrRef[prop][_prop][__prop].slice(start, end));
 							}
 						} else {
-							resultObj[prop][_prop][__prop] = arrRef[prop][_prop][__prop].slice(start, end);
+							resultObj[prop][_prop][__prop] = arrRef[prop][_prop][__prop];
 						}
 					});
 				}
@@ -78,17 +71,19 @@ const slice = (arrRef, start, end): IStatsChunk => {
 
 const collector: Collector = new Collector({ saveToFile: true });
 
-for (let i = 0; i < 10; i++) { // let's make 10 steps, like 10 seconds
-	for (let j = 0; j < Object.getOwnPropertyNames(file).length; j++) {
-		// emulate slicing, like customer sends data for each second per call
-		const chunkRefs: { [prop: string]: IStatsChunk } = file;
-		const currentChunk: IStatsChunk = chunkRefs[`${i}`];
-		const perSecondChunk: IStatsChunk = slice(currentChunk, i, i + 1);
+for (const connId of Object.getOwnPropertyNames(file)) {
+  const currentChunk: IStatsChunk = file[connId];
+  for (let i = 0; i < 10; i++) {
+		let perSecondChunk: IStatsChunk = slice(currentChunk, i, i + 1);
+    perSecondChunk = Object.assign({}, perSecondChunk, { connId });
+    collector.push(perSecondChunk);
+  }
 
-		// now pass that chunk to sdk
-		collector.push(perSecondChunk);
-	}
 }
 
-// collector.dropFile();
-// collector.dropFile();
+collector.readFromFile().then(
+  res => {
+    debugger;
+    console.log(`Done`);
+  }
+);
--- sample/collector/Collector.ts
+++ sample/collector/Collector.ts
@@ -2,6 +2,7 @@ import { ICollector } from './ICollector';
 import { IStatsChunk } from '../../interfaces/IUpdateStats';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as readline from 'readline';
 
 export class Collector implements ICollector {
   private _stack: IStatsChunk[] = [];
@@ -34,7 +35,8 @@ export class Collector implements ICollector {
 
 	private dropToFile(): boolean {
 		try {
-			const _chunk: any = `${JSON.stringify(this.get())}\n`;
+      const data: IStatsChunk[] = this._stack.slice();
+			const _chunk: any = `${JSON.stringify(data[data.length - 1])}\n`;
 			fs.appendFileSync(this._filePath, _chunk);
 		} catch (err) {
 			console.log(`Writing file exception: ${err}`);
@@ -43,6 +45,80 @@ export class Collector implements ICollector {
 
 		return true;
 	}
+
+  async readFromFile( filePath: string = this._filePath): Promise<any> {
+    let lines: any;
+    let readStream: fs.ReadStream;
+
+    if (!filePath) {
+      throw new Error(`No file path has been passed into readFromFile`);
+    }
+
+    try {
+      readStream = fs.createReadStream(filePath);
+    } catch (err) {
+      console.log(`Error creating read stream to ${filePath}: ${err}`);
+    }
+
+    try {
+      lines = readline.createInterface({ input: readStream });
+    } catch (err) {
+      console.log(`Error reading line from file: ${err}`);
+    }
+
+    return new Promise( (resolve, reject) => {
+      const _base: any = <IStatsChunk>{};
+      lines
+        .on('line', async line => {
+          const parsedLine: IStatsChunk = JSON.parse(line);
+          // here should be function which creates object for sending
+          this.processCollectedData(_base, parsedLine);
+        })
+        .on('close', () => {
+          resolve(_base);
+        });
+    });
+
+  }
+
+  // TODO: implement this
+  private processCollectedData(base: IStatsChunk, chunk: IStatsChunk): void {
+    const merge = (base: IStatsChunk, chunk: IStatsChunk): void => {
+      for (let prop of Object.getOwnPropertyNames(chunk)) {
+        for (let _prop of Object.getOwnPropertyNames(chunk[prop])) {
+          if (!Object.prototype.hasOwnProperty.call(base, prop)) { // new prop
+            base[prop] = chunk[prop];
+          } else { // merge prop content with base
+            if (!Object.prototype.hasOwnProperty.call(base[prop], _prop)) {
+              base[prop][_prop] = chunk[prop][_prop];
+            } else {
+              if (Array.isArray(chunk[prop][_prop])) {
+                Array.prototype.push.apply(base[prop][_prop], chunk[prop][_prop]);
+              } else {
+                // seems like we do not have non array props for merging
+                // above we assign new props, those shouldn't be changed
+              }
+            }
+          }
+        }
+      }
+    };
+
+    debugger;
+    if (!(chunk.connId in base)) {
+      base[chunk.connId] = <IStatsChunk>{
+        extra: {},
+        stat: {},
+        time: []
+      };
+    }
+
+    ['extra', 'stat'].forEach( prop => {
+      merge(base[chunk.connId][prop], chunk[prop]);
+    });
+
+    merge(base[chunk.connId]['time'], chunk['time']);
+  }
 
 	dropFile(): boolean {
 		try {
